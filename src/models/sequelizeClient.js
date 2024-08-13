7e2d98dbe314ad33148222509cc1c176dc190501
Heike13 <matthias.hibon@sfr.fr>
2024-08-13 16:05:46 +0200

    refaco client sequelize

--- src/models/sequelizeClient.js
+++ src/models/sequelizeClient.js
@@ -1,15 +1,12 @@
 import { Sequelize } from 'sequelize';
 
-function getConnexion() {
-    return new Sequelize(process.env.PG_URL, {
-        define: {
-            createdAt: 'created_at',
-            updatedAt: 'updated_at',
-            underscored: true,
-        },
+export const sequelize =  new Sequelize(process.env.PG_URL, {
+    define: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+        underscored: true,
+    },
 
-        logging: false,
-    });
-}
+    logging: false,
+});
 
-export { getConnexion };
