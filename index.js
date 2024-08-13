e53d1807f5b165002658119d81c6ed84f8dbae4a
Heike13 <matthias.hibon@sfr.fr>
2024-08-13 14:12:06 +0200

    refacto cors middleware

--- index.js
+++ index.js
@@ -9,20 +9,16 @@ import xssMiddleware from './src/middlewares/xssMiddleware.js';
 import express from 'express';
 const app = express();
 
-app.use(
-    cors({
-        origin: [
+const corsOptions = {
+    origin: function (origin, callback) {
+        const allowedOrigins = [
             'http://localhost',
             'http://localhost:3000',
             'http://127.0.0.1:3000',
-        ],
-        credentials: true,
-    })
-);
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin || origin !== -1) {
+        ];
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true);
+             
         } else {
             callback(new Error('Not allowed by CORS'));
         }
@@ -47,9 +43,11 @@ const { generateToken, validateRequest, doubleCsrfProtection } = doubleCsrf({
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limite de 100 requêtes par fenêtre de 15 minutes
-    standardHeaders: true, // Retourne les informations de limite dans les en-têtes `RateLimit-*`
-    legacyHeaders: false, // Désactive les en-têtes `X-RateLimit-*`
+    max: 100, // Limit each IP to 100 requests per windowMs
+    message:
+        'Trop de requêtes envoyées depuis cette adresse IP, veuillez réessayer après 15 minutes',
+    standardHeaders: true, // Returns the default rate limit headers with the response
+    legacyHeaders: false, // Desactivate the X-RateLimit headers
 });
 
 app.use(express.static('./dist'));
@@ -61,7 +59,7 @@ app.use(cookieParser());
 app.use(limiter);
 app.use(xssMiddleware);
 
-//  Route pour générer un token CSRF
+//  Route to generate a token CSRF
 app.get('/token', (req, res) => {
     try {
         if (!req.headers['x-csrf-token']) {
@@ -74,6 +72,7 @@ app.get('/token', (req, res) => {
         console.log(error.message);
     }
 });
+
 app.use(router, validateRequest);
 
 // Then launch the server
